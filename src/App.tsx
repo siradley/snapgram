2361f23e78b66705d5853befffa62f8ef56f4761
Sir Adley <74910825+siradley@users.noreply.github.com>
2024-01-15 14:07:36 +0800

    install routing, config routing, add layout page, sample pages (auth and home)
    - install react-router-dom
    - add layout page for each public and private routes
    - configure routing using Routes and Route
    - add sample pages (login, register, and home page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './globals.css'
+import { Routes, Route } from 'react-router-dom'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+import LoginForm from './_auth/forms/LoginForm'
+import RegisterForm from './_auth/forms/RegisterForm' // can put like { Home } below if you define something in index.ts maybe?
+import { Home } from './_root/pages'
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <main className="flex h-screen">
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />} >
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/register" element={<RegisterForm />} />
+                </Route>
+                
+                {/* private routes */}
+                <Route element={<RootLayout />} >
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
     )
 }
 
